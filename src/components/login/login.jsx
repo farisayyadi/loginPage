8db8a233428531ca6eebdfa83eebf413372277c7
Farideh Sayyadi <far.sayyadi@gmail.com>
2023-02-08 10:14:44 +0200

    get the name and save it in the cookie

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,25 +1,29 @@
 import React, {useState} from "react";
-
+//import SetCookie from "../cookies/setCookie";
+//import GetCookie from "../cookies/getCookie";
+//import RemoveCookie from "../cookies/removeCookie";
 
 
  const Login = () =>  {
 
-    const [name, setName] = useState('');
+    const [name, setName] = useState(''); 
 
     
-    
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        console.log(name)
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        document.cookie = `name=${name}`;
+
     }
-    const onChangeHandler = event => {
+
+    const handleChange = event => {
         setName(event.target.value);
     }
+
         return(
             <div>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="name"> UserName</label>
-                <input onChange={onChangeHandler} value={name} id="login_name" type="text" name="name" placeholder="write your name" required />
+                <input onChange={handleChange} value={name} id="login_name" type="text" name="name" placeholder="write your name" required />
                 <button type="submit">Submit</button>
             </form>
         </div>
